b366985fc97c3642e9f930fb6c9650b46eee119e
長徳　将希 <mchotoku@mbp-13jat-348.yjoffice.local>
2019-04-22 12:00:33 +0900

    refactor

--- knn/util.py
+++ knn/util.py
@@ -10,9 +10,10 @@ def get_args():
     parser = argparse.ArgumentParser(description='線形回帰で未来チャートの予想をします')
 
     parser.add_argument("--input", "-i", help="入力ファイル csv", default="../data/USDJPY_minute_20190104.csv")
-    parser.add_argument("--output", "-o", help="出力ファイル",
-                        default="output/result{}.txt".format(int(time.mktime(datetime.datetime.now().timetuple()))))
-    parser.add_argument("--minute", "-m", help="何分後の値を予想するか", type=int, default=120)
+    parser.add_argument("--outpath", "-o", help="出力ファイル",
+                        default="output".format(int(time.mktime(datetime.datetime.now().timetuple()))))
+    parser.add_argument("--minute", "-M", help="何分後の値を予想するか", type=int, default=30)
+    parser.add_argument("--model", "-m", help="モデルのdumpデータのpath", default="./model.pickle")
     return parser.parse_args()
 
 
@@ -89,11 +90,11 @@ def add_technical_values(_table, _moving_average_list=[5, 21, 34, 144]):
     return _table
 
 
-def generate_explanatory_variables(_table, _day_ago, _technical_num):
-    ret_table = np.zeros((len(_table), _day_ago * _technical_num))
+def generate_explanatory_variables(_table, _learn_minute_ago, _technical_num):
+    ret_table = np.zeros((len(_table), _learn_minute_ago * _technical_num))
     for s in range(0, _technical_num):  # 日にちごとに横向きに並べる
-        for i in range(0, _day_ago):
-            ret_table[i:len(_table), _day_ago * s + i] = _table[0:len(_table) - i, s + 4]
+        for i in range(0, _learn_minute_ago):
+            ret_table[i:len(_table), _learn_minute_ago * s + i] = _table[0:len(_table) - i, s + 4]
     return ret_table
 
 
@@ -117,7 +118,7 @@ def normalize(_x, _y, _day_ago):
     return _x, _y
 
 
-def get_result(Y_test, Y_pred, output_path, result):
+def get_result(Y_test, Y_pred, out_tsv_path):
     # 正答率を計算
     correct_num = 0
     for i in range(len(Y_pred)):
@@ -128,25 +129,33 @@ def get_result(Y_test, Y_pred, output_path, result):
     # 前々日終値に比べて前日終値が高い場合は、買いとする
     reward = 0
 
+    entry_num = 0
+    entry_correct_num = 0
     for i in range(0, len(Y_test)):
-        if Y_pred[i] >= 0:
+        if Y_pred[i] >= 0.05:
             reward += Y_test[i]
-        else:
+            entry_num += 1
+            if Y_test[i] >= 0:
+                entry_correct_num += 1
+        if Y_pred[i] <= -0.05:
             reward -= Y_test[i]
+            entry_num += 1
+            if Y_test[i] <= 0:
+                entry_correct_num += 1
 
-    with open(output_path, "w", encoding="utf-8") as f:
-        f.write("予測：")
+    with open(out_tsv_path, "w", encoding="utf-8") as f:
+        f.write("予測")
         for pred in Y_pred:
             f.write("\t" + str(round(pred, 4)))
-        f.write("\n実際：")
+        f.write("\n実際")
         for test in Y_test:
             f.write("\t" + str(round(test, 4)))
             # f.write("実際\t{0}".format(Y_test))
 
-    print("予測数：{0}\t正解数：{1}\t正解率：{2:.3f}\t利益合計：{3:.3f}".format(
-        len(Y_pred), correct_num, correct_num / len(Y_pred) * 100, reward))
+    print("予測数: {0}\t正解率: {1:.3f}\tエントリー数: {2}\tエントリー正解率: {3:.3f}\t利益合計：{4:.3f}".format(
+        len(Y_pred), correct_num / len(Y_pred) * 100, entry_num, entry_correct_num / entry_num * 100, reward))
 
-    return len(Y_pred), correct_num, reward
+    return len(Y_pred), correct_num, entry_num, entry_correct_num, reward
 
     # # 予測結果の総和グラフを描くーーーーーーーーー
     # total_return = np.zeros(len(Y_test))
