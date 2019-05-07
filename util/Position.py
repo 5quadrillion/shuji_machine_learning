03a0d96dc1a6329823f04d7cdeb0e2f7276cf92c
shuji <shuji.lab@gmail.com>
2019-05-07 21:43:32 +0900

    position classを作成

--- util/Position.py
+++ util/Position.py
@@ -1,29 +1,87 @@
 # coding=utf-8
-class Position:
-    __pos = 0
-    __max_pos = 0
-    __min_pos = 0
 
-    def __init__(self):
-        pass
 
-    def buy(self):
-        self.__pos += 1
-        if self.__max_pos < self.__pos:
-            self.__max_pos = self.__pos
+# 買いポジション
+class BidPosition:
+    # 買値
+    __bid_price = 0
+    # 損切りポイント
+    __loss_cut_price = 0
+    # 利確ポイント
+    __profit_price = 0
 
-    def sell(self):
-        self.__pos -= 1
-        if self.__min_pos > self.__pos:
-            self.__min_pos = self.__pos
+    def __init__(self, bid_price, loss_cut_price, profit_price):
+        self.__bid_price = bid_price
+        self.__loss_cut_price = loss_cut_price
+        self.__profit_price = profit_price
 
-    def get_max_pos(self):
-        return self.__max_pos
+    # 利確か損切りポイントにかかっていたら決済する
+    def settle_if_need(self, now_price):
+        if now_price < self.__loss_cut_price:
+            print("Loss cut!!")
+            return self.__loss_cut_price - self.__bid_price
+        if self.__profit_price < now_price:
+            print("Set profits!!")
+            return self.__profit_price - self.__bid_price
+        return None
 
-    def get_min_pos(self):
-        return self.__min_pos
 
-    def clear(self):
-        self.__pos = 0
-        self.__min_pos = 0
-        self.__max_pos = 0
+# 売りポジション
+class AskPosition:
+    # 売り値
+    __ask_price = 0
+    # 損切りポイント
+    __loss_cut_price = 0
+    # 利確ポイント
+    __profit_price = 0
+
+    def __init__(self, ask_price, loss_cut_price, profit_price):
+        self.__ask_price = ask_price
+        self.__loss_cut_price = loss_cut_price
+        self.__profit_price = profit_price
+
+    # 利確か損切りポイントにかかっていたら決済する
+    def settle_if_need(self, now_price):
+        if self.__loss_cut_price < now_price:
+            print("Loss cut!!")
+            return self.__ask_price - self.__loss_cut_price
+        if now_price < self.__profit_price:
+            print("Set profits!!")
+            return self.__ask_price - self.__profit_price
+        return None
+
+
+class Positions:
+    __positions = []
+    __loss_cut_pips = 20
+    __profit_pips = 50
+    __profit_and_loss = 0
+
+    def __init__(self, loss_cut_pips, profit_pips):
+        self.__loss_cut_pips = loss_cut_pips
+        self.__profit_pips = profit_pips
+
+    def add_bid_position(self, bid_price):
+        pos = BidPosition(bid_price=bid_price, loss_cut_price=(bid_price-self.__loss_cut_pips*0.01), profit_price=(bid_price+self.__profit_pips*0.01))
+        self.__positions.append(pos)
+
+    def add_ask_position(self, ask_price):
+        pos = AskPosition(ask_price=ask_price, loss_cut_price=(ask_price+self.__loss_cut_pips*0.01), profit_price=(ask_price-self.__profit_pips*0.01))
+        self.__positions.append(pos)
+
+    def position_check(self, start, high, low, end):
+        if start < end:
+            # 本来は違うかもしれないが、start -> low -> high -> end と移り変わったと過程
+            check_price = [start, low, high, end]
+        else:
+            # 本来は違うかもしれないが、start -> high -> low -> end と移り変わったと過程
+            check_price = [start, high, low, end]
+        for pos in self.__positions[:]:
+            for cp in check_price:
+                val = pos.settle_if_need(cp)
+                if val is not None:
+                    self.__profit_and_loss += val
+                    self.__positions.remove(pos)
+
+    def get_profit_and_loss(self):
+        return self.__profit_and_loss
